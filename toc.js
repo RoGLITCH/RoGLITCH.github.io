74bda77dda7a2081766a2e940175f3498c8b5724
RoGLITCH <60105496@udst.edu.qa>
2025-09-02 04:01:07 -0400

    update

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="HackTheBox/Windows/Easy/Lock.html"><strong aria-hidden="true">1.</strong> Lock</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="HackTheBox/Windows/Easy/Active.html"><strong aria-hidden="true">1.</strong> Active</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
